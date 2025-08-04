8880b59f4965c99f073ce8db6fe5e158ed033ffc
marcosihombing72 <marcosihombing72@gmail.com>
2025-08-05 03:51:51 +0700

    update

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -9,12 +9,12 @@ import {
 import * as dayjs from 'dayjs';
 import * as ExcelJS from 'exceljs';
 import { PassThrough } from 'stream';
-import { LoginAdminDto } from './admin/dto/login-admin.dto';
-import { LogoutAdminDto } from './admin/dto/logout-admin.dto';
-import { PeranAdmin, RegisterAdminDto } from './admin/dto/register-admin.dto';
-import { ResetPasswordDto } from './admin/dto/reset-password-admin.dto';
-import { UpdateAdminProfileDto } from './admin/dto/update-admin.dto';
-import { supabase } from './supabase/supabase.client';
+import { supabase } from '../supabase/supabase.client';
+import { LoginAdminDto } from './dto/login-admin.dto';
+import { LogoutAdminDto } from './dto/logout-admin.dto';
+import { PeranAdmin, RegisterAdminDto } from './dto/register-admin.dto';
+import { ResetPasswordDto } from './dto/reset-password-admin.dto';
+import { UpdateAdminProfileDto } from './dto/update-admin.dto';
 const PDFDocument = require('pdfkit');
 
 @Injectable()

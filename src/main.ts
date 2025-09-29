45c3f0f8c01c0e084f795dc7ac78e067550fef96
marco-sihombing <injapri@gmail.com>
2025-09-29 08:07:21 +0700

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import { AppModule } from '@/app.module';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import express from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   // Security & performance setup
-  const server = app.getHttpAdapter().getInstance();
   server.set('trust proxy', 1);
   app.use(helmet());
   app.use(
     rateLimit({
-      windowMs: 60 * 1000, // 1 menit
-      max: 100, // max 100 request per menit
+      windowMs: 60 * 1000,
+      max: 100,
     }),
   );
 
@@ -24,17 +27,19 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // Swagger setup
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('Buku Tamu MKG')
     .setDescription('Buku Tamu MKG')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;

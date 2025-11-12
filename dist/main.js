d26bfabc39b6bf80230102e6dd93acd314b4d87e
marco-sihombing <injapri@gmail.com>
2025-11-12 14:04:16 +0700

    feat: update

--- dist/main.js
+++ dist/main.js
@@ -12,35 +12,23 @@ const express_1 = __importDefault(require("express"));
 const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const helmet_1 = __importDefault(require("helmet"));
 const server = (0, express_1.default)();
+server.use((0, cors_1.default)({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+}));
+server.options('*', (0, cors_1.default)());
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
     server.set('trust proxy', 1);
     app.use((0, helmet_1.default)());
-    app.use((0, express_rate_limit_1.default)({
-        windowMs: 60 * 1000,
-        max: 100,
-    }));
+    app.use((0, express_rate_limit_1.default)({ windowMs: 60 * 1000, max: 100 }));
     app.setGlobalPrefix('api');
-    app.enableCors({
-        origin: true,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-        credentials: true,
-    });
-    server.use((0, cors_1.default)({
-        origin: true,
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-        credentials: true,
-    }));
-    server.options('*', (0, cors_1.default)({ origin: true, credentials: true }));
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Buku Tamu MKG')
         .setDescription('Buku Tamu MKG')
         .setVersion('1.0')
-        .addBearerAuth({
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-    }, 'access-token')
+        .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token')
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api', app, document);

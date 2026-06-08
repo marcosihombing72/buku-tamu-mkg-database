27e02488f33139819cbcd8ef70d5c765f65bb33f
marco-sihombing <injapri@gmail.com>
2026-06-08 21:40:08 +0700

    fix: 304

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,43 @@
 import { AppModule } from '@/app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  // Security & performance setup
+  server.set('trust proxy', 1);
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 60 * 1000,
+      max: 100,
+    }),
+  );
+  app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store');
+    next();
+  });
+
+  // jadi /api
+  app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
+  // Swagger
   const config = new DocumentBuilder()
-    .setTitle('Admin API')
-    .setDescription('Dokumentasi API untuk manajemen admin')
+    .setTitle('Buku Tamu MKG')
+    .setDescription('Buku Tamu MKG')
     .setVersion('1.0')
     .addBearerAuth(
       {
@@ -35,10 +48,12 @@ async function bootstrap() {
       'access-token',
     )
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
